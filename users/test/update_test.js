1c22d779bc1ce705d1b2a83dbc2b5c3382321c8a
Ryan Pfister <Ryan Pfister>
2017-02-02 21:10:31 -0700

    testing testing testing

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
     let joe;
 
     beforeEach((done) => {
-        joe = new User({ name: 'Joe' });
+        joe = new User({ name: 'Joe', postCount: 0 });
         joe.save()
             .then(() => {
                 done();
@@ -32,15 +32,24 @@ describe('Updating records', () => {
     });
 
     it('A model class can update', done => {
-        assertName(User.update({name: 'Joe'}, {name: 'Alex'}), done);
+        assertName(User.update({ name: 'Joe' }, { name: 'Alex' }), done);
     });
 
     it('A model class can update one record', done => {
-        assertName(User.findOneAndUpdate({name: 'Joe'}, {name: 'Alex'}), done);
+        assertName(User.findOneAndUpdate({ name: 'Joe' }, { name: 'Alex' }), done);
     });
 
     it('A model class can update a record with an Id and update', done => {
-        assertName(User.findByIdAndUpdate(joe._id, {name: 'Alex'}), done);
+        assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+    });
+
+    it("a user can have their postcount incremented by 1", (done) => {
+        User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+            .then(() => User.findOne({ name: 'Joe' }))
+            .then(user => {
+                assert(user.postCount === 1);
+                done();
+            });
     });
 
 });
