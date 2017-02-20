42aaacec7e0a4b3d5ce44172aafb197af86c77ba
Ryan Pfister <Ryan Pfister>
2017-02-20 00:51:09 -0700

    dev

---
+++ muber/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/muber');
+
+app.use(bodyParser.json());
+routes(app);
+
+module.exports = app;
